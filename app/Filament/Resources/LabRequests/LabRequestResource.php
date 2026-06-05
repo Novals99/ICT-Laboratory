8926d7ca334cab3e8e7bbbe14ed9fea5fdad1d68
Ahmad Mulia Aliansyah <2511501203@student.budiluhur.ac.id>
2026-06-05 10:03:54 +0700

    uptade resource comment

--- app/Filament/Resources/LabRequests/LabRequestResource.php
+++ app/Filament/Resources/LabRequests/LabRequestResource.php
@@ -16,6 +16,7 @@ use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
 
+// Update Git profile contribution
 class LabRequestResource extends Resource
 {
     protected static ?string $navigationLabel = 'Lab Request';
