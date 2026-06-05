7dc51ef4d16ab9cf5024b2c2646a833c2ac1f8c4
Ali <2511501203@gmail.com>
2026-06-05 09:39:23 +0700

    export button

--- app/Filament/Resources/LabRequests/Pages/ListLabRequests.php
+++ app/Filament/Resources/LabRequests/Pages/ListLabRequests.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\LabRequests\Pages;
 
 use App\Filament\Resources\LabRequests\LabRequestResource;
 use Filament\Actions\CreateAction;
+use Filament\Actions\Action;
 use Filament\Resources\Pages\ListRecords;
 
 class ListLabRequests extends ListRecords
@@ -13,6 +14,12 @@ class ListLabRequests extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
+            Action::make('export')
+                ->label('Export ')
+                ->icon('heroicon-o-document-arrow-down')
+                ->url(route('labrequest.export'))
+                ->openUrlInNewTab(),
+
             CreateAction::make(),
         ];
     }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LabRequestPdfController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/labrequest/export', [LabRequestPdfController::class, 'export'])
+    ->name('labrequest.export');
