3897e6ff9df3c89e44207abfaa550f75ee8fd367
ccheryll1 <rylche663@gmail.com>
2026-06-20 02:51:44 +0700

    refactor: Remove unused CSS and Blade component files

--- resources/views/components/auth-session-status.blade.php
+++
@@ -1,7 +0,0 @@
-@props(['status'])
-
-@if ($status)
-    <div {{ $attributes->merge(['class' => 'mb-4 text-sm font-medium text-green-600 dark:text-green-400']) }}>
-        {{ $status }}
-    </div>
-@endif
--- resources/views/components/text-input.blade.php
+++
@@ -1,6 +0,0 @@
-@props(['disabled' => false])
-
-<input
-    @disabled($disabled)
-    {{ $attributes->merge(['class' => 'rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-indigo-600 dark:focus:ring-indigo-600']) }}
->
