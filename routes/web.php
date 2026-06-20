d279b1fb1ed8dd0b9118e8b4e1c0ad35849bb7ba
Noval Sanjaya <novalzecros@gmail.com>
2026-06-21 06:32:56 +0700

    fix: correct dashboard route definition by adding middleware

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect()->route('login');
 });
-Route::get('/dashboard', DashboardController::class)->middleware('auth')->name('dashboard');
 Route::middleware('auth')->group(function () {
 
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
