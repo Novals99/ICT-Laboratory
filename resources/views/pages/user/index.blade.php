989db02f48d7fbd7ae7cd709e43b7089d289f46c
ccheryll1 <rylche663@gmail.com>
2026-06-23 03:17:50 +0700

    fix: enhance delete confirmation message and update form handling in user table

--- resources/views/pages/user/index.blade.php
+++ resources/views/pages/user/index.blade.php
@@ -157,26 +157,27 @@
                                     @csrf
                                     @method('DELETE')
 
-                                    <x-table.action
-                                        type="button"
-                                        variant="delete"
-                                        title="Delete"
-                                        onclick="window.dispatchEvent(new CustomEvent('open-confirm', {
-                                            detail: {
-                                                title: 'Delete User?',
-                                                message: 'Are you sure want to delete {{ $user->name }}?',
-                                                formId: 'delete-user-{{ $user->id }}'
-                                            }
-                                        }))"
-                                    >
-                                        <svg class="h-4 w-4" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                                            stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round">
-                                            <polyline points="3 6 5 6 21 6" />
-                                            <path d="M19 6l-1 14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2L5 6" />
-                                            <path d="M10 11v6M14 11v6" />
-                                            <path d="M9 6V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v2" />
-                                        </svg>
-                                    </x-table.action>
+                                        <x-table.action
+                                                type="button"
+                                                variant="delete"
+                                                title="Delete"
+                                                onclick="window.dispatchEvent(new CustomEvent('open-confirm', {
+                                                    detail: {
+                                                        title: 'Delete User?',
+                                                        message: 'Are you sure you want to delete {{ addslashes($user->name) }}?',
+                                                        form: this.closest('form'),
+                                                        type: 'delete'
+                                                    }
+                                                }))"
+                                            >
+                                                <svg class="h-4 w-4" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                    stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round">
+                                                    <polyline points="3 6 5 6 21 6" />
+                                                    <path d="M19 6l-1 14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2L5 6" />
+                                                    <path d="M10 11v6M14 11v6" />
+                                                    <path d="M9 6V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v2" />
+                                                </svg>
+                                            </x-table.action>
                                 </form>
                             </div>
                         </x-table.td>
