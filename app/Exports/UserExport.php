1f917b56b39a0849f1e66f5e8d638bd3b7845c33
ccheryll1 <rylche663@gmail.com>
2026-06-24 00:09:07 +0700

    feat: update UserExport to include user ID and correct lab name retrieval for export

--- app/Exports/UserExport.php
+++ app/Exports/UserExport.php
@@ -10,14 +10,14 @@ class UserExport extends BaseExport
     public function collection(): Collection
     {
         return User::with('labs')
-            ->select('name', 'nim', 'role', 'username', 'status_user', 'email')
+            ->select('id', 'name', 'nim', 'role', 'username', 'status_user', 'email')
             ->get()
             ->map(function ($user) {
                 return [
                     'name'        => $user->name,
                     'nim'         => $user->nim,
                     'role'        => $user->role,
-                    'labs'        => $user->labs->pluck('name')->join(', '),
+                    'labs'        => $user->labs->pluck('lab_name')->join(', ') ?: '-',
                     'username'    => $user->username,
                     'status_user' => $user->status_user,
                     'email'       => $user->email,
